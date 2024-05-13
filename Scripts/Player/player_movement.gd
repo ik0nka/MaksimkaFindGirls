f8bd5364c5e6ac58de115284151274de11973f65
ik0nka <tachitsce2@gmail.com>
2024-05-13 22:06:21 +0300

    0.2.2
    -новый максик
    -фон для меню(начало(пока хуйня))

--- Scripts/Player/player_movement.gd
+++ Scripts/Player/player_movement.gd
@@ -1,14 +1,17 @@
 extends CharacterBody2D
 
 var max_speed = 20
+@export var physics : bool = true
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
 	velocity.x = lerp(velocity.x, 0.0, 0.2) 
 	var direction = Input.get_action_strength("Right") - Input.get_action_strength("Left")
 
-	velocity.x += direction * max_speed 
-	velocity.y += gravity * delta
+	velocity.x += direction * max_speed
+
+	if physics:
+		velocity.y += gravity * delta
 
 	_animate(direction)
 	move_and_slide()
@@ -17,7 +20,7 @@ func _animate(direction):
 	if direction == 0:
 		$AnimatedSprite2D.play("idle")
 	else:
-		# $AnimatedSprite2D.play("run")
+		$AnimatedSprite2D.play("run")
 		if direction < 0:
 			$AnimatedSprite2D.flip_h = true
 		elif direction > 0: 
